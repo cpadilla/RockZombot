2f1ca1aa02d46908b0d91a9e8c024d0f7c779a53
cpadilla <christoferpadilla@gmail.com>
2018-03-10 18:44:20 -0500

    Working on laying out the main button

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="menu">
+        <v-container fluid grid-list-md>
+            <v-layout row>
+                <v-flex md12>
+                    <v-spacer>
+                        <v-card v-for="i in 4" :key="i">
+                            This will contain menu items
+                        </v-card>
+                    </v-spacer>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="viewer">
-    <router-view/>
-    <v-app>
-      <v-flex grid-list-md text-xs-right>
-        <div id="buttonContainer">
-          <v-btn color="success" >>_</v-btn>
-        </div>
-      </v-flex>
+    <!-- <router-view/> -->
+    <v-app dark class="transparent">
+        <v-container grid-list-md>
+          <v-layout row fill-height align-end justify-end>
+            <v-flex xs4>
+              <Menu v-show="expanded"/>
+              <v-layout class="endRow" row justify-end>
+                <v-flex xs6 align-items-end>
+                  <v-btn v-on:click="expanded = !expanded" color="success" >{{ buttonText }}</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
     </v-app>
   </div>
 </template>
 
 <script>
+import Menu from './Menu'
+
 export default {
-  name: 'Viewer'
+  name: 'Viewer',
+  components: {
+    Menu
+  },
+  data() {
+    return {
+      expanded: false,
+      buttonText: ">_"
+    }
+  }
 }
 </script>
 
@@ -27,13 +45,8 @@ $variable: 100px;
   text-align: center;
   color: #2c3e50;
 
-  // v-app 
-  .application {
-    background: transparent;
-
-    // #buttonContainer {
-      
-    // }
+  .endRow {
+    padding-bottom: 40px;
   }
 }
 </style>
