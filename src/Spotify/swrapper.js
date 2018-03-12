504a280b68506530776b733f2736213f3ff40307
= <=>
2018-03-12 16:32:21 -0400

    Playing songs support

--- src/Spotify/swrapper.js
+++ src/Spotify/swrapper.js
@@ -1,24 +1,44 @@
 const request = require('request-promise')
 
-export default {
+
+var token = "BQBJmkVerYyIQcy6wPeI71V__KBppX4sZ9BQUJZkA1E0OG_Zazx3bZuS6-kHGSbwgmz1tL9ooIeOpIRnrIT6lWJZlTTEMuC1YmUdgNgra09C_bqAxY9KT4WIXQt2lGKmaxQUCLKgY2AchvKkxXLDjAPH6IV68NVTxxtYQ2D6D6v_FedS_2oIz4S8JAvp-KYZPlgCgddjNabNaeAg8nf0QxbqwTeL5bPv_OdkTftXSrPIECVjyBNGYwS7SZ6kiCmEI4z1IyQ";
+module.exports = {
   playSong: function getSong(song){
-    if(song.indexOf(' ') >= 0){
+    if(song.indexOf(' ') > 0){
       song.replace(" ", "%20")
     }
     var options = {
       url: `https://api.spotify.com/v1/search?q=${song}&type=track&market=us&limit=1`,
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json'
       }
     }
     return promise = new Promise(function (resolve, reject){
       request(options).then(function(data){
         var trackInfo = JSON.parse(data);
-        resolve(trackInfo.tracks.items[0].external_urls.spotify);
+        //console.log(trackInfo.tracks.items[0].uri);
+        var options_new = {
+          url: "https://api.spotify.com/v1/me/player/play",
+          method: "PUT",
+          json: { "uris": [trackInfo.tracks.items[0].uri]},
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        };
+        request(options_new).then(function(success){resolve("Request Succesfully Sent!")}).catch(function(error){reject(error)})
+        //resolve(trackInfo.tracks.items[0].uri);
       }).catch(function(error){
         reject(error);
       });
+    });
+  },
+  play: function play(){
+
+    return promise = new Promise(function(resolve, reject){
+      request(options_new).then(function(success){resolve(success)}).catch(function(error){reject(error)});
     });
   },
   nextSong: function nextSong(){
